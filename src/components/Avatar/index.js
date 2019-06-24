485b95e6e7620da0bf7434f89a283cbd1604f86e
LeoTianJob <leo.tian@ap.jll.com>
2019-06-24 22:23:46 +0800

    add axios and fetch user's profile information.

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -5,7 +5,10 @@ import styles from './styles';
 
 const avatar = () => {
     return (
-        <Image source={{ uri: 'https://via.placeholder.com/150/92c952'}} style={{width: 50, height: 50}}/>
+        <Image 
+            source={{ uri: 'https://via.placeholder.com/150/92c952'}} 
+            style={{width: 50, height: 50}}
+        />
     );
 }
 
--- src/containers/Profile/index.js
+++ src/containers/Profile/index.js
@@ -1,20 +1,38 @@
 import React, { Component } from 'react';
 import { Text, SafeAreaView } from 'react-native';
+import axios from 'axios';
 
 import Avatar from '../../components/Avatar';
 
 class Profile extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            profileInfo: null
+        }
+    }
+
     componentDidMount() {
         // Assume the current login user's id = 1
-        
+        axios.get('https://jsonplaceholder.typicode.com/users/1')
+            .then(res => {
+                const { data } = res;
+                if (this.state.profileInfo === null){
+                    this.setState({ profileInfo: data})
+                }
+            })
+            .catch(error => {
+                console.log('Fetching user infomation failed')
+            })
     }
 
     render() {
         return (
             <SafeAreaView>
                 <Avatar />
-                <Text>Test</Text>
+                <Text>{JSON.stringify(this.state.profileInfo)}</Text>
             </SafeAreaView>
         );
     }
