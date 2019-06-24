2e402b99d1d24bccae35dba3359d11c4fa8531c6
LeoTianJob <leo.tian@ap.jll.com>
2019-06-25 05:36:34 +0800

    add redux-axios-middleware.

---
+++ src/store/root.js
@@ -0,0 +1,16 @@
+const initState = {
+    loginUser: {
+        id: null,
+
+    }
+}
+
+const reducer = (state = initState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+
+};
+
+export default reducer;
