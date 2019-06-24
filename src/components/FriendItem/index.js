f1df994be61bae10bc7a6538a44748259605af76
LeoTianJob <leo.tian@ap.jll.com>
2019-06-25 03:13:57 +0800

    add FriendItem component.

--- src/components/FriendItem/index.js
+++ src/components/FriendItem/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
+import PropTypes from 'prop-types';
 
 import Avatar from '../../components/Avatar';
 
@@ -13,11 +14,23 @@ const friendItem = ({ name, address, onClick }) => {
         <TouchableOpacity style={container} onPress={onClick} >
             <Avatar /> 
             <View style={userInfoStyle}>
-                <Text style={itemStyle}>USER NAME</Text>
-                <Text style={itemStyle}>USER ADDRESS</Text>
+                <Text style={itemStyle}>{name}</Text>
+                <Text style={itemStyle}>{address}</Text>
             </View>
         </TouchableOpacity>
     );
+}
+
+friendItem.propTypes = {
+    name: PropTypes.string,
+    address: PropTypes.string,
+    onClick: PropTypes.func
+}
+
+friendItem.defaultProps = {
+    name: 'USER NAME',
+    address: 'USER ADDRESS',
+    onClick: () => {}
 }
 
 export default friendItem;
