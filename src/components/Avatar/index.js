c6719d7f2c3ecc494a47867a467b7b7e6a14d9ea
LeoTianJob <leo.tian@ap.jll.com>
2019-06-24 22:36:20 +0800

    finish Avatar component.

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Image } from 'react-native';
 
-import styles from './styles';
+const avatar = ({ imgUri = null, size = 64}) => {
+    const source = imgUri ? { uri: imgUri} : require('../../assets/images/user.png');
 
-const avatar = () => {
     return (
         <Image 
-            source={{ uri: 'https://via.placeholder.com/150/92c952'}} 
-            style={{width: 50, height: 50}}
+            source={source} 
+            style={{width: size, height: size, borderRadius: size / 2}}
         />
     );
 }
