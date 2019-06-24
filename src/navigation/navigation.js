82b1ccbca332e5d6601c8a2ccd5142b0e91bce5c
LeoTianJob <leo.tian@ap.jll.com>
2019-06-25 02:17:17 +0800

    add simple colors.

--- src/navigation/navigation.js
+++ src/navigation/navigation.js
@@ -1,12 +1,16 @@
 // import React from 'react';
 import { Navigation } from 'react-native-navigation';
-import registerScreens, { POSTS_PAGE, FRIENDS_PAGE, ALBUMS_PAGE, TODOS_PAGE, PROFILE_PAGE} from './screens';
+import registerScreens, { POSTS_PAGE, FRIENDS_PAGE, ALBUMS_PAGE, TODOS_PAGE, PROFILE_PAGE } from './screens';
 import Icon from 'react-native-vector-icons/FontAwesome';
+
 import { defaultFont } from '../styles/fonts';
+import { honoluluBlue, etonBlue, americanGreen } from '../styles/colors'
 
 const ICON_SIZE = 25;
-const BUTTON_TEXT_COLOR = 'black';
+const BUTTON_TEXT_COLOR = etonBlue;
+const BUTTON_TEXT_SELECTED_COLOR = americanGreen;
 const BUTTON_FONT_SIZE = 10;
+const TOPBAR_TEXT_COLOR = honoluluBlue;
 const TOPBAR_FONT_SIZE = 20;
 
 registerScreens();
@@ -34,136 +38,161 @@ export default initAppScreens = () => {
                 bottomTabs: {
                     id: 'BottomTabsId',
                     children: [
-                    {
-                        stack: {
-                            children: [{
-                                component: {
-                                    name: POSTS_PAGE,
-                                    options: {
-                                        topBar: {
-                                            title: {
-                                                text: 'Latest Posts',
-                                                fontFamily: defaultFont,
-                                                fontSize: TOPBAR_FONT_SIZE,
+                        {
+                            stack: {
+                                children: [{
+                                    component: {
+                                        name: POSTS_PAGE,
+                                        options: {
+                                            topBar: {
+                                                title: {
+                                                    text: 'Latest Posts',
+                                                    fontFamily: defaultFont,
+                                                    fontSize: TOPBAR_FONT_SIZE,
+                                                    color: TOPBAR_TEXT_COLOR
+                                                }
                                             }
                                         }
                                     }
-                                }
-                            }],
-                            options: {
-                                bottomTab:{
-                                    fontSize: BUTTON_FONT_SIZE,
-                                    fontFamily: defaultFont,
-                                    text: POSTS_PAGE,
-                                    icon: postsIcon
+                                }],
+                                options: {
+                                    bottomTab: {
+                                        fontSize: BUTTON_FONT_SIZE,
+                                        fontFamily: defaultFont,
+                                        text: POSTS_PAGE,
+                                        icon: postsIcon,
+                                        textColor: BUTTON_TEXT_COLOR,
+                                        iconColor: BUTTON_TEXT_COLOR,
+                                        selectedTextColor: BUTTON_TEXT_SELECTED_COLOR,
+                                        selectedIconColor: BUTTON_TEXT_SELECTED_COLOR,
+                                    }
                                 }
                             }
-                        }
-                    },
-                    {
-                        stack: {
-                            children: [{
-                                component: {
-                                    name: FRIENDS_PAGE,
-                                    options: {
-                                        topBar: {
-                                            title: {
-                                                text: 'Friends',
-                                                fontFamily: defaultFont,
-                                                fontSize: TOPBAR_FONT_SIZE
+                        },
+                        {
+                            stack: {
+                                children: [{
+                                    component: {
+                                        name: FRIENDS_PAGE,
+                                        options: {
+                                            topBar: {
+                                                title: {
+                                                    text: 'Friends',
+                                                    fontFamily: defaultFont,
+                                                    fontSize: TOPBAR_FONT_SIZE,
+                                                    color: TOPBAR_TEXT_COLOR
+                                                }
                                             }
                                         }
                                     }
-                                }
-                            }],
-                            options: {
-                                bottomTab:{
-                                    fontSize: BUTTON_FONT_SIZE,
-                                    fontFamily: defaultFont,
-                                    text: FRIENDS_PAGE,
-                                    icon: friendsIcon
+                                }],
+                                options: {
+                                    bottomTab: {
+                                        fontSize: BUTTON_FONT_SIZE,
+                                        fontFamily: defaultFont,
+                                        text: FRIENDS_PAGE,
+                                        icon: friendsIcon,
+                                        textColor: BUTTON_TEXT_COLOR,
+                                        iconColor: BUTTON_TEXT_COLOR,
+                                        selectedTextColor: BUTTON_TEXT_SELECTED_COLOR,
+                                        selectedIconColor: BUTTON_TEXT_SELECTED_COLOR,
+                                    }
                                 }
                             }
-                        }
-                    },
-                    {
-                        stack: {
-                            children: [{
-                                component: {
-                                    name: ALBUMS_PAGE,
-                                    options: {
-                                        topBar: {
-                                            title: {
-                                                text: 'Albums',
-                                                fontFamily: defaultFont,
-                                                fontSize: TOPBAR_FONT_SIZE
+                        },
+                        {
+                            stack: {
+                                children: [{
+                                    component: {
+                                        name: ALBUMS_PAGE,
+                                        options: {
+                                            topBar: {
+                                                title: {
+                                                    text: 'Albums',
+                                                    fontFamily: defaultFont,
+                                                    fontSize: TOPBAR_FONT_SIZE,
+                                                    color: TOPBAR_TEXT_COLOR
+                                                }
                                             }
                                         }
                                     }
-                                }
-                            }],
-                            options: {
-                                bottomTab:{
-                                    fontSize: BUTTON_FONT_SIZE,
-                                    fontFamily: defaultFont,
-                                    text: ALBUMS_PAGE,
-                                    icon: albumsIcon
+                                }],
+                                options: {
+                                    bottomTab: {
+                                        fontSize: BUTTON_FONT_SIZE,
+                                        fontFamily: defaultFont,
+                                        text: ALBUMS_PAGE,
+                                        icon: albumsIcon,
+                                        textColor: BUTTON_TEXT_COLOR,
+                                        iconColor: BUTTON_TEXT_COLOR,
+                                        selectedTextColor: BUTTON_TEXT_SELECTED_COLOR,
+                                        selectedIconColor: BUTTON_TEXT_SELECTED_COLOR,
+                                    }
                                 }
                             }
-                        }
-                    },
-                    {
-                        stack: {
-                            children: [{
-                                component: {
-                                    name: TODOS_PAGE,
-                                    options: {
-                                        topBar: {
-                                            title: {
-                                                text: 'Todo List',
-                                                fontFamily: defaultFont,
-                                                fontSize: TOPBAR_FONT_SIZE
+                        },
+                        {
+                            stack: {
+                                children: [{
+                                    component: {
+                                        name: TODOS_PAGE,
+                                        options: {
+                                            topBar: {
+                                                title: {
+                                                    text: 'Todo List',
+                                                    fontFamily: defaultFont,
+                                                    fontSize: TOPBAR_FONT_SIZE,
+                                                    color: TOPBAR_TEXT_COLOR
+                                                }
                                             }
                                         }
                                     }
-                                }
-                            }],
-                            options: {
-                                bottomTab:{
-                                    fontSize: BUTTON_FONT_SIZE,
-                                    fontFamily: defaultFont,
-                                    text: TODOS_PAGE,
-                                    icon: todosIcon
+                                }],
+                                options: {
+                                    bottomTab: {
+                                        fontSize: BUTTON_FONT_SIZE,
+                                        fontFamily: defaultFont,
+                                        text: TODOS_PAGE,
+                                        icon: todosIcon,
+                                        textColor: BUTTON_TEXT_COLOR,
+                                        iconColor: BUTTON_TEXT_COLOR,
+                                        selectedTextColor: BUTTON_TEXT_SELECTED_COLOR,
+                                        selectedIconColor: BUTTON_TEXT_SELECTED_COLOR,
+                                    }
                                 }
                             }
-                        }
-                    },
-                    {
-                        stack: {
-                            children: [{
-                                component: {
-                                    name: PROFILE_PAGE,
-                                    options: {
-                                        topBar: {
-                                            title: {
-                                                text: 'Profile',
-                                                fontFamily: defaultFont,
-                                                fontSize: TOPBAR_FONT_SIZE
+                        },
+                        {
+                            stack: {
+                                children: [{
+                                    component: {
+                                        name: PROFILE_PAGE,
+                                        options: {
+                                            topBar: {
+                                                title: {
+                                                    text: 'Profile',
+                                                    fontFamily: defaultFont,
+                                                    fontSize: TOPBAR_FONT_SIZE,
+                                                    color: TOPBAR_TEXT_COLOR
+                                                }
                                             }
                                         }
                                     }
-                                }
-                            }],
-                            options: {
-                                bottomTab:{
-                                    fontSize: BUTTON_FONT_SIZE,
-                                    fontFamily: defaultFont,
-                                    text: PROFILE_PAGE,
-                                    icon: profileIcon
+                                }],
+                                options: {
+                                    bottomTab: {
+                                        fontSize: BUTTON_FONT_SIZE,
+                                        fontFamily: defaultFont,
+                                        text: PROFILE_PAGE,
+                                        icon: profileIcon,
+                                        textColor: BUTTON_TEXT_COLOR,
+                                        iconColor: BUTTON_TEXT_COLOR,
+                                        selectedTextColor: BUTTON_TEXT_SELECTED_COLOR,
+                                        selectedIconColor: BUTTON_TEXT_SELECTED_COLOR,
+                                    }
                                 }
                             }
                         }
-                    }
                     ]
                 }
             }
--- src/styles/colors.js
+++ src/styles/colors.js
@@ -1,3 +1,5 @@
+export const pureWhite = '#ffffff';
+export const pureBlack = '#000000'
 export const iceberg = '#78ADD2';
 export const honoluluBlue = '#0072AA';
 export const etonBlue = '#A4D0A0';
