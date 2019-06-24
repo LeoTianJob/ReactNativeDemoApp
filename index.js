51a75feb994022178c8d7a68849d530cba277cd0
LeoTianJob <leo.tian@ap.jll.com>
2019-06-25 00:04:17 +0800

    add icons to tab buttons

--- index.js
+++ index.js
@@ -2,74 +2,88 @@
  * @format
  */
 
+import React from 'react';
 import { Navigation } from 'react-native-navigation';
 import registerScreens from './src/navigators/screens';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 registerScreens();
 
 Navigation.events().registerAppLaunchedListener(() => {
-    Navigation.setRoot({
-        root: {
-            bottomTabs: {
-                id: 'BottomTabsId',
-                children: [{
-                    component: {
-                        name: 'Posts',
-                        options: {
-                            bottomTab:{
-                                fontSize: 12,
-                                text: 'Posts'
+    Promise.all([
+        Icon.getImageSource('newspaper-o', 30, 'black'),
+        Icon.getImageSource('users', 30, 'black'),
+        Icon.getImageSource('image', 30, 'black'),
+        Icon.getImageSource('list', 30, 'black'),
+        Icon.getImageSource('user', 30, 'black')
+    ]).then((icons) => {
+        Navigation.setRoot({
+            root: {
+                bottomTabs: {
+                    id: 'BottomTabsId',
+                    children: [{
+                        component: {
+                            name: 'Posts',
+                            options: {
+                                bottomTab:{
+                                    fontSize: 12,
+                                    text: 'Posts',
+                                    icon: icons[0]
+                                }
+                            }
+                        }
+                    },
+                    {
+                        component: {
+                            name: 'Friends',
+                            options: {
+                                bottomTab:{
+                                    fontSize: 12,
+                                    text: 'Friends',
+                                    icon: icons[1]
+                                }
+                            }
+                        }
+                    },
+                    {
+                        component: {
+                            name: 'Albums',
+                            options: {
+                                bottomTab:{
+                                    fontSize: 12,
+                                    text: 'Albums',
+                                    icon: icons[2]
+                                }
+                            }
+                        }
+                    },
+                    {
+                        component: {
+                            name: 'Todos',
+                            options: {
+                                bottomTab:{
+                                    fontSize: 12,
+                                    text: 'Todos',
+                                    icon: icons[3]
+                                }
+                            }
+                        }
+                    },
+                    {
+                        component: {
+                            name: 'Profile',
+                            options: {
+                                bottomTab:{
+                                    fontSize: 12,
+                                    text: 'Profile',
+                                    icon: icons[4]
+                                }
                             }
                         }
                     }
-                },
-                {
-                    component: {
-                        name: 'Friends',
-                        options: {
-                            bottomTab:{
-                                fontSize: 12,
-                                text: 'Friends'
-                            }
-                        }
-                    }
-                },
-                {
-                    component: {
-                        name: 'Albums',
-                        options: {
-                            bottomTab:{
-                                fontSize: 12,
-                                text: 'Albums'
-                            }
-                        }
-                    }
-                },
-                {
-                    component: {
-                        name: 'Todos',
-                        options: {
-                            bottomTab:{
-                                fontSize: 12,
-                                text: 'Todos'
-                            }
-                        }
-                    }
-                },
-                {
-                    component: {
-                        name: 'Profile',
-                        options: {
-                            bottomTab:{
-                                fontSize: 12,
-                                text: 'Profile',
-                                // icon: <PostsIcon width={32} height={32} />
-                            }
-                        }
-                    }
+                    ]
                 }
-                ]
             }
-        }
-    });
+        });
+    })
 });
--- src/containers/Posts/index.js
+++ src/containers/Posts/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Text, SafeAreaView } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 class Posts extends Component {
     render() {
