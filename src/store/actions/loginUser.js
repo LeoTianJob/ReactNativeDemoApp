ccad0c4ea8b8b2987bf21febe9c82e1014bd661e
LeoTianJob <leo.tian@ap.jll.com>
2019-06-26 11:54:55 +0800

    add explanation.

--- src/store/actions/loginUser.js
+++ src/store/actions/loginUser.js
@@ -11,12 +11,17 @@ export const getLoginUser = (userId) => {
     };
 }
 
+// This query method is incorrect from data structure perspective, 
+// in order to load online images for user's profile,
+// for simplify the query, I just force to use photo whose id equals to user id as user's profile image
+// the correct way is find out the user's album and pick one photo from there.
+
 export const getLoginUserImg = (userId) => {
     return {
         type: GET_LOGIN_USER_IMG,
         payload: {
             request: {
-                url: `/photos?id=${userId}`
+                url: `/photos?id=${userId}` 
             }
         }
     }
