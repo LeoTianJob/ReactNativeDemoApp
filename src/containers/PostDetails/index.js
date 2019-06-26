9eb19dc7d03bc2e19599d2c662a515d2f7393a96
LeoTianJob <leo.tian@ap.jll.com>
2019-06-26 09:42:32 +0800

    add PostDetail Page.

---
+++ src/containers/PostDetails/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+
+class PostDetails extends Component {
+
+    render() {
+        return <Text />
+    }
+
+}
+
+export default PostDetails;
--- src/navigation/screens.js
+++ src/navigation/screens.js
@@ -6,12 +6,14 @@ import configureStore from '../store/configureStore';
 import withSafeArea from '../hoc/withSafeArea';
 
 import Posts from '../containers/Posts';
+import PostDetails from '../containers/PostDetails';
 import Friends from '../containers/Friends';
 import Albums from '../containers/Albums';
 import Todos from '../containers/Todos';
 import Profile from '../containers/Profile';
 
 export const POSTS_PAGE = 'Posts';
+export const POST_DETAILS = 'PostDetails';
 export const FRIENDS_PAGE = 'Friends';
 export const ALBUMS_PAGE = 'Albums';
 export const TODOS_PAGE = 'Todos';
@@ -22,6 +24,7 @@ const store = configureStore();
 // Only TODOS and PROFILE support redux, I made this on purpose, for other pages, I will use local state
 registerScreens = () => {
     Navigation.registerComponent(POSTS_PAGE, () => withSafeArea(Posts));
+    Navigation.registerComponent(POST_DETAILS, () => PostDetails);
     Navigation.registerComponent(FRIENDS_PAGE, () => withSafeArea(Friends));
     Navigation.registerComponent(ALBUMS_PAGE, () => withSafeArea(Albums));
     Navigation.registerComponentWithRedux(TODOS_PAGE, () => withSafeArea(Todos), Provider, store);
