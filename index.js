1968b0868452dd5371fa6e39639e968b639c7d64
LeoTianJob <leo.tian@ap.jll.com>
2019-06-27 02:35:43 +0800

    remove useless code.

--- index.js
+++ index.js
@@ -5,8 +5,4 @@
 import { Navigation } from 'react-native-navigation';
 import initAppScreens from './src/navigation/navigation';
 
-import { Provider, createStore, applyMiddleware, combineReducers } from 'redux';
-import axios from 'axios';
-import axiosMiddleware from 'redux-axios-middleware';
-
 Navigation.events().registerAppLaunchedListener(() => initAppScreens());
